d1db22a97845ded5894922f32e1504abbd6ad7a7
Marcos Melo <suiciniv2016+github@outlook.com>
2025-07-18 14:24:52 -0300

    fix(shared-domain): remove unused get-drizzle-transaction-token factory

--- libs/shared/domain/src/lib/factories/get-drizzle-transaction-token.ts
+++
@@ -1,8 +0,0 @@
-import { getTransactionToken } from '@nestjs-cls/transactional';
-
-// TODO: Replace with new import
-import { DRIZZLE_CONNECTION_NAME } from '../../infrastructure/database/drizzle';
-
-export function getDrizzleTransactionToken() {
-  return getTransactionToken(DRIZZLE_CONNECTION_NAME);
-}
--- libs/shared/domain/src/lib/factories/index.ts
+++ libs/shared/domain/src/lib/factories/index.ts
@@ -1,2 +1,1 @@
-export * from './get-drizzle-transaction-token.js';
 export * from './get-repository-token.js';
