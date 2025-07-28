ad59fcc92a8e156f24dbde22dd6ee3cfefb1a2db
ronaldemanuel <emronald15el@gmail.com>
2025-07-28 18:38:14 -0300

    chore: last configs from item application

--- libs/item/application/src/lib/item-application.module.ts
+++ libs/item/application/src/lib/item-application.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 
 import { commands } from './commands/index.js';
 import { queries } from './queries/index.js';
+import { AddItemToInventoryUseCase } from './use-cases/add-item-to-inventory.use-case.js';
+import { ListAllItemsUseCase } from './use-cases/list-all-items.use-case.js';
+import { ListUserInventoryUseCase } from './use-cases/list-user-inventory.use-case.js';
 import { UseItemUseCase } from './use-cases/use-item.use-case.js';
 
 @Module({
-  providers: [...commands, ...queries, UseItemUseCase.UseCase],
-  exports: [UseItemUseCase.UseCase],
+  providers: [
+    ...commands,
+    ...queries,
+    UseItemUseCase.UseCase,
+    AddItemToInventoryUseCase.UseCase,
+    ListUserInventoryUseCase.UseCase,
+    ListAllItemsUseCase.UseCase,
+  ],
+  exports: [
+    UseItemUseCase.UseCase,
+    AddItemToInventoryUseCase.UseCase,
+    ListUserInventoryUseCase.UseCase,
+    ListAllItemsUseCase.UseCase,
+  ],
 })
 export class ItemApplicationModule {}
--- libs/item/application/src/lib/use-cases/index.ts
+++ libs/item/application/src/lib/use-cases/index.ts
@@ -1,1 +1,4 @@
 export * from './use-item.use-case.js';
+export * from './add-item-to-inventory.use-case.js';
+export * from './list-user-inventory.use-case.js';
+export * from './list-all-items.use-case.js';
