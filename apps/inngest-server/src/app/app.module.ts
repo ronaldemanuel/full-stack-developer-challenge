42bbd128ba1c442274b4744c5587c178b0fbbf5c
ronaldemanuel <emronald15el@gmail.com>
2025-08-02 16:43:13 -0300

    feat: config server

--- apps/inngest-server/src/app/app.module.ts
+++ apps/inngest-server/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { InngestModule } from 'nest-inngest';
+
+import { inngest } from '@nx-ddd/jobs-events-presentation';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [InngestModule.forRoot({ inngest, path: '/api/inngest' })],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/inngest-server/src/main.ts
+++ apps/inngest-server/src/main.ts
@@ -1,43 +1,10 @@
-import type { NestExpressApplication } from '@nestjs/platform-express';
-import { inngest } from '@modules/common/inngest/client';
-import { getInngestFunctions } from '@modules/common/inngest/functions';
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { serve } from 'inngest/express';
 
 import { AppModule } from './app/app.module';
-import { AppService } from './app/app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    bodyParser: true,
-  });
-
-  // Setup inngest
-  app.useBodyParser('json', { limit: '10mb' });
-
-  // Inject Dependencies into inngest functions
-
-  const logger = app.get(Logger);
-  const appService = app.get(AppService);
-
-  // Pass dependencies into this function
-  const inngestFunctions = getInngestFunctions({
-    appService,
-    logger,
-  });
-
-  // Register inngest endpoint
-  app.use(
-    '/api/inngest',
-    serve({
-      client: inngest,
-      functions: inngestFunctions,
-    }),
-  );
-
-  // Start listening for http requests
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3030);
 }
 
 bootstrap();
